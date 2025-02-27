e8cd5ad837098095c57e7e6daa337c6d2482d337
Hemanth0411 <hemanthreddyannem@gmail.com>
2025-02-27 15:33:07 +0530

    Initial commit for dev branch

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class UserProfile(AbstractUser):
+    # Add any additional fields here
+    pass
+
